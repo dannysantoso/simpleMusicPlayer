444ba1c7b5995b56e045b7cc5032be6c09a55e3a
dannysantoso <danny.sntoso@gmail.com>
2020-05-08 21:52:21 +0700

    Initial Commit

---
+++ musicPlaylist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  musicPlaylist
+//
+//  Created by danny santoso on 08/05/20.
+//  Copyright © 2020 danny santoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
